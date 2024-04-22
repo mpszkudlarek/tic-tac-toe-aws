fe4f1f62804e57d505c67cbc461f39d5f3b492da
Michał Szkudlarek <michal1szkudlarek@gmail.com>
2024-04-22 15:58:55 +0200

    Update Terraform comments

--- deployment/main.tf
+++ deployment/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "tictactoe_tf" {
-  ami = "ami-04e5276ebb8451442" # ID of the Amazon Machine Image to use for the instance
-  instance_type = "t2.small" # Specifies the instance type
+  ami = "ami-04e5276ebb8451442" # ID of the Amazon Machine Image to use for the instance, ami-04e5276ebb8451442 is the Amazon Linux 2 AMI
+  instance_type = "t2.small" # Specifies the instance type, t2.small is a general-purpose instance type
   key_name = "key_flask_vue" # SSH key name for instance authentication
   associate_public_ip_address = true # Determines if a public IP should be assigned
   vpc_security_group_ids = [aws_security_group.security_group_tf.id] # Security group to attach
@@ -19,8 +19,8 @@ resource "aws_instance" "tictactoe_tf" {
 
 resource "aws_vpc" "vpc_tf" {
   cidr_block = "10.0.0.0/16" # Defines the IP address range for the VPC, in CIDR notation, so it can hold 65,536 IP addresses from 10.0.0.0 to 10.0.255.555
-  enable_dns_support = true # Enables DNS support within the VPC
-  enable_dns_hostnames = true # Enables DNS hostname resolution within the VPC
+  enable_dns_support = true # Enables DNS support within the VPC, so instances can resolve domain names
+  enable_dns_hostnames = true # Enables DNS hostname resolution within the VPC, so instances can have DNS hostnames
 
   tags = {
     Name = "VPC TF" # Name tag for the VPC
@@ -28,7 +28,7 @@ resource "aws_vpc" "vpc_tf" {
 }
 
 resource "aws_subnet" "subnet_tf" {
-  vpc_id = aws_vpc.vpc_tf.id # Associates the subnet with the VPC
+  vpc_id = aws_vpc.vpc_tf.id # Associates the subnet with the VPC, so it can use the VPC's IP address range
   cidr_block = "10.0.1.0/24" # Defines the subnets IP address range, in CIDR notation, so it can hold 256 IP addresses from 10.0.1.0 to 10.01.255
 
   tags = {
@@ -37,7 +37,7 @@ resource "aws_subnet" "subnet_tf" {
 }
 
 resource "aws_internet_gateway" "gateway_tf" {
-  vpc_id = aws_vpc.vpc_tf.id # Associates the internet gateway with the VPC
+  vpc_id = aws_vpc.vpc_tf.id # Associates the internet gateway with the VPC, so it can route traffic to and from the internet
 
   tags = {
     Name = "Gateway TF" # Name tag for the internet gateway
@@ -45,7 +45,7 @@ resource "aws_internet_gateway" "gateway_tf" {
 }
 
 resource "aws_route_table" "route_table_tf" {
-  vpc_id = aws_vpc.vpc_tf.id # Associates the route table with the VPC
+  vpc_id = aws_vpc.vpc_tf.id # Associates the route table with the VPC, so it can route traffic within the VPC
 
   route {
     cidr_block = "0.0.0.0/0" # Directs all traffic
@@ -58,8 +58,8 @@ resource "aws_route_table" "route_table_tf" {
 }
 
 resource "aws_route_table_association" "subnet_tf" {
-  subnet_id = aws_subnet.subnet_tf.id # Associates the subnet with the route table
-  route_table_id = aws_route_table.route_table_tf.id # Associates the route table with the subnet
+  subnet_id = aws_subnet.subnet_tf.id # Associates the subnet with the route table, so it can route traffic within the subnet
+  route_table_id = aws_route_table.route_table_tf.id # Associates the route table with the subnet, so it can route traffic within the VPC
 }
 
 resource "aws_security_group" "security_group_tf" {
